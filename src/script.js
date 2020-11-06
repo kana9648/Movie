721fcb8781bf2e702813fb41c88041294f9c8743
kana9648 <67888717+kana9648@users.noreply.github.com>
2020-11-06 15:37:48 +0900

    1

--- src/script.js
+++ src/script.js
@@ -43,12 +43,12 @@
       alert("이메일을 입력하세요.");
     } else if (exptext.test(email) == false) {
       alert("이메일 형식이 틀립니다.");
-    } else if (!/^[a-zA-Z0-9]{10,15}$/.test(pass_1)) {
-      alert("숫자와 영문자 조합으로 10~15자리를 사용해야 합니다.");
+    } else if (!/^[a-zA-Z0-9]{8,15}$/.test(pass_1)) {
+      alert("비밀번호는 숫자와 영문자 조합으로 8~15자리를 사용해야 합니다.");
     } else if (checkNumber < 0 || checkEnglish < 0) {
-      alert("숫자와 영문자를 혼용하여야 합니다.");
+      alert("비밀번호는 숫자와 영문자를 혼용하여야 합니다.");
     } else if (/(\w)\1\1\1/.test(pass_1)) {
-      alert("같은 문자를 4번 이상 사용하실 수 없습니다.");
+      alert("비밀번호는 같은 문자를 4번 이상 사용하실 수 없습니다.");
     } else if (pass_1.search(id) > -1) {
       alert("비밀번호에 아이디가 포함되었습니다.");
     }else{
