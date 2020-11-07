5df257062007da3bdc5b686a8b66921eb63f7e0e
kana9648 <67888717+kana9648@users.noreply.github.com>
2020-11-08 05:08:02 +0900

    Add files via upload

---
+++ src/alertwrite.js
@@ -0,0 +1,3 @@
+function alertwrite(){
+    alert('미구현 기능입니다');
+}
---
+++ src/popup.js
@@ -0,0 +1,10 @@
+function popup(){
+    var url = "./popup.html";
+    var name = "popup test";
+    var option = "width = 450, height = 330, top = 100, left = 200, location = no"
+    window.open(url, name, option);
+    
+}
+function alert(){
+    alert('미구현 기능입니다');
+}
