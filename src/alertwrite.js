8f73a91b198ddecdc1198efd62dc2c52092a7746
kana9648 <67888717+kana9648@users.noreply.github.com>
2020-11-12 05:54:01 +0900

    1

--- src/alertwrite.js
+++
@@ -1,3 +0,0 @@
-function alertwrite(){
-    alert('미구현 기능입니다');
-}
--- src/read.js
+++
@@ -1,4 +0,0 @@
-let num = Math.floor(Math.random() * 5)+1;
-let img = document.getElementById(`img`);
-
-img.src = `img/m${num}.jpg`;
