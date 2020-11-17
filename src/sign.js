fbf669c8c99c6fe826671b26cee9a3871fe13259
kana9648 <67888717+kana9648@users.noreply.github.com>
2020-11-17 18:10:53 +0900

    1

--- src/sign.js
+++ src/sign.js
@@ -57,6 +57,10 @@
     document.getElementById("com").style.display = "block";
     }
   }
+  function printName()  {
+    const name = document.getElementById('name').value;
+    document.getElementsByClassName("resultname").innerText = name;
+  }
 
     let num = Math.floor(Math.random() * 5)+1;
     let img = document.getElementById(`img`);
